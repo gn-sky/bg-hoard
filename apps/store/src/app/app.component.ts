9de08846c165c663c5f9030ffe0670d80cf50a76
Eugene <eugene.sky.dev@gmail.com>
2023-02-16 12:12:32 -0600

    before running migration

---
+++ apps/store/src/app/app.component.ts
@@ -0,0 +1,22 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'bg-hoard-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css'],
+// })
+// export class AppComponent {
+//   title = 'store';
+// }
+import { Component } from '@angular/core';
+import { getAllGames } from '../fake-api';
+
+@Component({
+  selector: 'bg-hoard-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Board Game Hoard';
+  games = getAllGames();
+}
